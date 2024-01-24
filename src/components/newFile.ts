71e2b46d967eb55af17d2a9d2ac859ddcaaefe55
Matheus Malena <matheusmalena28@gmail.com>
2024-01-24 12:24:43 -0300

    mudança para abrir no git hub pages

--- src/components/newFile.ts
+++ src/components/newFile.ts
@@ -1,9 +1,11 @@
 import type ICategoria from '@/interfaces/ICategoria';
 import type { PropType } from 'vue';
+import IngredienteSelecionavel from './IngredienteSelecionavel.vue'
+import Tag from './Tag.vue';
 
 export default (await import('vue')).defineComponent({
-props: {
-categoria: { type: Object as PropType<ICategoria>, required: true }
-},
-components: { TagVue, IngredienteSelecionavelVue }
+    props: {
+        categoria: { type: Object as PropType<ICategoria>, required: true }
+    },
+    components: { Tag, IngredienteSelecionavel }
 });
