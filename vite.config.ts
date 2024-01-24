40fbac7da0be74a9df41db7017c969c5d415cd57
Matheus Malena <matheusmalena28@gmail.com>
2024-01-24 13:37:00 -0300

    projeto atualizado

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/nomedoseurepositorio/',
+  base: '/cookin-up/',
   plugins: [vue()],
   resolve: {
     alias: {
