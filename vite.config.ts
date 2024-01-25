18b00b2b87733c13aa30dc0325d935f81b59ff1b
Matheus Malena <matheusmalena28@gmail.com>
2024-01-25 14:58:29 -0300

    Atualizar submódulo dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': new URL('./src', import.meta.url).pathname
     }
   },
   publicPath: process.env.NODE_ENV === 'production'
